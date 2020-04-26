bfdd353c0a0e14729c198508261f19f0ca67322e
Entor Arifi <entor.arifi@jaywalker-digital.ch>
2020-04-26 11:58:55 +0200

    Render multiple elements.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // JSX
-ReactDOM.render(<h1>Hello</h1>, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+        <h1>Hello</h1>
+        <p>This is my paragraph</p>
+    </div>,
+    document.getElementById('root')
+);
 
 // var h1 = document.createElement('h1');
 // h1.innerText = 'Hello';
