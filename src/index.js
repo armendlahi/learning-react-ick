c2c74d9c7b6a4296f22f6579eb6ad5e1e856b68c
Entor Arifi <entor.arifi@jaywalker-digital.ch>
2020-04-26 11:32:54 +0200

    ReactDOM.render and JSX.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// JSX
+ReactDOM.render(<h1>Hello</h1>, document.getElementById('root'));
+
+// var h1 = document.createElement('h1');
+// h1.innerText = 'Hello';
+// document.getElementById('root').appendChild(h1)
