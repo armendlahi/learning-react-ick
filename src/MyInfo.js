261ad768aeddd1d360cfd785a1fe8ce7badcf09f
Entor Arifi <entor.arifi@jaywalker-digital.ch>
2020-04-26 12:32:30 +0200

    Moved MyInfo component to a separate file.

---
+++ src/MyInfo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>Entor Arifi</h1>
+            <p>I am Entor Arifi and I enjoy writing react applications.</p>
+
+            <h4>My favorite languages</h4>
+            <ul>
+                <li>JavaScript</li>
+                <li>Golang</li>
+                <li>Java</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyInfo;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function MyInfo() {
-    return (
-        <div>
-            <h1>Entor Arifi</h1>
-            <p>I am Entor Arifi and I enjoy writing react applications.</p>
-
-            <h4>My favorite languages</h4>
-            <ul>
-                <li>JavaScript</li>
-                <li>Golang</li>
-                <li>Java</li>
-            </ul>
-        </div>
-    )
-}
+import MyInfo from './MyInfo';
 
 ReactDOM.render(<MyInfo />, document.getElementById('root'));
