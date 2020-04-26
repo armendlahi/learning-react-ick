c43841a008ab4fbe90bcc3e3033af8094207a4ed
Entor Arifi <entor.arifi@jaywalker-digital.ch>
2020-04-26 12:08:21 +0200

    Moved the list to a functional component.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function MyList() {
+    return (
+        <ul>
+            <li>First</li>
+            <li>Second</li>
+            <li>Third</li>
+            <li>Other</li>
+        </ul>
+    );
+}
+
 // JSX
 ReactDOM.render(
-    <ul>
-        <li>First</li>
-        <li>Second</li>
-        <li>Third</li>
-    </ul>,
+    <MyList />,
     document.getElementById('root')
 );
 
