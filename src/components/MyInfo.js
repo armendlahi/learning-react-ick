6de71ba9841d0a62c1e177b7d16a15a34817205e
Entor Arifi <entor.arifi@jaywalker-digital.ch>
2020-04-28 17:54:52 +0200

    Use template strings inside JSX.

--- src/components/MyInfo.js
+++ src/components/MyInfo.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const MyInfo = () => {
-    const firstName = 'Entorasd'; 
+    const firstName = 'Entor'; 
     const lastName = 'Arifi'; 
 
     return (
         <div>
-            <h1>{firstName + ' ' + lastName}</h1>
+            <h1>{`${firstName} ${lastName}`}</h1>
             <p>I am Entor Arifi and I enjoy writing react applications.</p>
 
             <h4>My favorite languages</h4>
