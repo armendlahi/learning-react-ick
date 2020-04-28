81dff1bcd26b937c80e34fa55de3e3ff00c4f218
Entor Arifi <entor.arifi@jaywalker-digital.ch>
2020-04-28 19:46:59 +0200

    Use object destructuring when accessing props.

--- src/components/Person.js
+++ src/components/Person.js
@@ -6,12 +6,14 @@ const Person = props => {
         height: 150
     };
 
+    const { name, avatar, dateOfBirth, email } = props.data;
+
     return (
         <div className="person">
-            <img src={props.data.avatar} alt={props.data.name} style={style} />
-            <h3>{props.data.name}</h3>
-            <h4>{props.data.dateOfBirth}</h4>
-            <h4><a href={`mailto:${props.data.email}`}>{props.data.email}</a></h4>
+            <img src={avatar} alt={name} style={style} />
+            <h3>{name}</h3>
+            <h4>{dateOfBirth}</h4>
+            <h4><a href={`mailto:${email}`}>{email}</a></h4>
             <hr />
         </div>
     );
