f50d1995da9b313e9006ba56da1ecfc013a0da45
Entor Arifi <entor.arifi@jaywalker-digital.ch>
2020-04-30 19:36:21 +0200

    Initialized state insde App.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 
-import Person from './components/Person';
-import persons from './providers/persons';
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            firstName: 'Jon Doe'
+        };
+    }
 
-const App = () => {
-    const myCompany = 'X Company';
-
-    const components = persons.map(person => <Person data={person} />);
-
-    return (
-        <div className="container">
-            <h1>HR App - {myCompany}</h1>
-            {components}
-        </div>
-    );
+    render() {
+        return (
+            <h1>{this.state.firstName}</h1>
+        )
+    }
 }
 
 export default App;
