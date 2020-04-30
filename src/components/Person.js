3232d0d98ae6e5e48eafa2f91554163cbe0554bb
Entor Arifi <entor.arifi@jaywalker-digital.ch>
2020-04-30 19:28:40 +0200

    Convert functional (stateless) component to a class-based component.

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
-const Person = ({ data: { name, avatar, dateOfBirth, email }}) => {
-    const style = {
-        width: 150,
-        height: 150
-    };
+class Person extends React.Component {
+    render() {
+        const style = {
+            width: 150,
+            height: 150
+        };
 
-    return (
-        <div className="person">
-            <img src={avatar} alt={name} style={style} />
-            <h3>{name}</h3>
-            <h4>{dateOfBirth}</h4>
-            <h4><a href={`mailto:${email}`}>{email}</a></h4>
-            <hr />
-        </div>
-    );
+        return (
+            <div className="person">
+                <img src={this.props.data.avatar} alt={this.props.data.name} style={style} />
+                <h3>{this.props.data.name}</h3>
+                <h4>{this.props.data.dateOfBirth}</h4>
+                <h4><a href={`mailto:${this.props.data.email}`}>{this.props.data.email}</a></h4>
+                <hr />
+            </div>
+        );
+    }
 };
 
 export default Person;
