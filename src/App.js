c0bf928533ffcaf8ede9c853bbd98c21ee497825
Entor Arifi <entor.arifi@jaywalker-digital.ch>
2020-04-30 20:00:41 +0200

    Conditional rendering using state.

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            firstName: 'John Doe'
-        };
+            kaKaluarProvimin: false
+        }
     }
 
-    render() {
-        return (
-            <div>
-                <h1>{this.state.firstName}</h1>
-            </div>
-        )
+    render () {
+        // let header;
+
+        // if (this.state.kaKaluarProvimin) {
+        //     header = <h1>Ju keni kaluar provmin</h1>;
+        // } else {
+        //     header = <h1>Ju nuk keni kaluar provmin</h1>;
+        // }
+
+        return <h1>{`Ju ${this.state.kaKaluarProvimin ? '' : 'nuk'} keni kaluar provimin`}</h1>;
+        // return this.state.kaKaluarProvimin ? <h1>Ju keni kaluar provmin</h1> : <h1>Ju nuk keni kaluar provimin</h1>;
     }
 }
 
