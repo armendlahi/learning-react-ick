3c22722a4c4e88f44b775c77035b5f20f5811cce
Entor Arifi <entor.arifi@jaywalker-digital.ch>
2020-05-02 10:22:09 +0200

    Event handlers with parameters.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-function doSomething() {
-    console.log('asdasd');
+function doSomething(element) {
+    console.log(`${element} clicked`);
 }
 
 function App() {
     return (
         <div>
-            <h1 onClick={() => console.log('header')}>My header</h1>
-            <p onClick={() => console.log('onClick paragraph')} onMouseOver={() => console.log('paragraph')}>Attachment apartments in delightful by motionless it no. And now she burst sir learn total. Hearing hearted shewing own ask. Solicitude uncommonly use her motionless not collecting age. The properly servants required mistaken outlived bed and. Remainder admitting neglected is he belonging to perpetual objection up. Has widen too you decay begin which asked equal any. </p>
+            <h1 onClick={() => doSomething('h1')}>My header</h1>
+            <p onClick={() => doSomething('p')}>Attachment apartments in delightful by motionless it no. And now she burst sir learn total. Hearing hearted shewing own ask. Solicitude uncommonly use her motionless not collecting age. The properly servants required mistaken outlived bed and. Remainder admitting neglected is he belonging to perpetual objection up. Has widen too you decay begin which asked equal any. </p>
         </div>
     )
 }
