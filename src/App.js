2a6ab1ed1a211cd053b23fb0e5d69709d7751195
Entor Arifi <entor.arifi@jaywalker-digital.ch>
2020-05-02 10:16:02 +0200

    Inline event handlers in react.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
 
-class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            kaKaluarProvimin: false
-        }
-    }
+function doSomething() {
+    console.log('asdasd');
+}
 
-    render () {
-        // let header;
-
-        // if (this.state.kaKaluarProvimin) {
-        //     header = <h1>Ju keni kaluar provmin</h1>;
-        // } else {
-        //     header = <h1>Ju nuk keni kaluar provmin</h1>;
-        // }
-
-        return <h1>{`Ju ${this.state.kaKaluarProvimin ? '' : 'nuk'} keni kaluar provimin`}</h1>;
-        // return this.state.kaKaluarProvimin ? <h1>Ju keni kaluar provmin</h1> : <h1>Ju nuk keni kaluar provimin</h1>;
-    }
+function App() {
+    return (
+        <div>
+            <h1 onClick={() => console.log('header')}>My header</h1>
+            <p onClick={() => console.log('onClick paragraph')} onMouseOver={() => console.log('paragraph')}>Attachment apartments in delightful by motionless it no. And now she burst sir learn total. Hearing hearted shewing own ask. Solicitude uncommonly use her motionless not collecting age. The properly servants required mistaken outlived bed and. Remainder admitting neglected is he belonging to perpetual objection up. Has widen too you decay begin which asked equal any. </p>
+        </div>
+    )
 }
 
 export default App;
