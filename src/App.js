cc2b33c7831748875d83025762ab8bb2f66c9270
Entor Arifi <entor.arifi@jaywalker-digital.ch>
2020-05-05 19:23:15 +0200

    Configure one event handler to work for multiple elements.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            firstName: 'Entor'
+            firstName: 'Entor',
+            lastName: 'Arifi',
+            email: 'arifientor@gmail.com'
         }
     }
     
     handleChange(event) {
+        const name = event.target.name;
+        const value = event.target.value;
+
         this.setState({
-            firstName: event.target.value
+            [name]: value
         });
     }
 
@@ -21,9 +26,17 @@ class App extends React.Component {
                 <form>
                     <div>
                         <label>First Name: </label>
-                        <input type="text" value={this.state.firstName} onChange={this.handleChange.bind(this)} />
+                        <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange.bind(this)} />
                     </div>
-                    <h2>{this.state.firstName}</h2>
+                    <div>
+                        <label>Last Name: </label>
+                        <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange.bind(this)} />
+                    </div>
+                    <div>
+                        <label>Email: </label>
+                        <input type="text" name="email" value={this.state.email} onChange={this.handleChange.bind(this)} />
+                    </div>
+                    <h2>{this.state.firstName} {this.state.lastName} {this.state.email}</h2>
                     <div>
                         <button type="submit">Submit</button>
                     </div>
