1f89f95fa15435d3c7b2a56b18b2e53821ca6d81
Entor Arifi <entor.arifi@jaywalker-digital.ch>
2020-05-05 17:01:49 +0200

    Authentication example.

--- src/App.js
+++ src/App.js
@@ -1,55 +1,31 @@
 import React from 'react';
 import randomColor from 'randomcolor';
 
-import ChildComponent from './ChildComponent';
+import Authentication from './Authentication';
 
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            counter: 0,
-            color: 'red'
+            loading: true
         }
     }
 
-    handleClick() {
-        this.setState(prevState => {
-            return {
-                counter: prevState.counter + 1
-            }
-        });
-    }
 
     componentDidMount() {
-        // This lifefycle method is used for initializing component data and/or API calls (fetching data from other sources)
-        console.log('Component did mount.');
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        // console.log('Should component udate.');
-        // return nextState.counter % 2 === 0;
-        return true; // default value
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        console.log(prevProps, prevState);
-        if (prevState.counter !== this.state.counter) {
-            this.setState({
-                color: randomColor()
-            });
-        }
+        setTimeout(() => {
+            this.setState({ loading: false})
+        }, 1500);
     }
 
     render() {
-        console.log('Render');
-
         return (
             <div>
-                <h1 style={{ color: this.state.color }}>{this.state.counter}</h1>
-                <button onClick={this.handleClick.bind(this)}>Change counter</button>
-                {this.state.counter < 2 && (
-                    <ChildComponent counter={this.state.counter} />
-                )}
+                <h1>Header</h1>
+                <hr />
+                {this.state.loading ? 'Loading...' : <Authentication username="MyUsername" />}
+                <hr />
+                <h1>Footer</h1>
             </div>
         )
     }
