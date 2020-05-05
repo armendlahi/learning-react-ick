afd4a96ba9007e6c5e0803aa7ba4c28117d40898
Entor Arifi <entor.arifi@jaywalker-digital.ch>
2020-05-05 19:50:13 +0200

    HTML Elements as controlled component.

--- src/App.js
+++ src/App.js
@@ -6,44 +6,71 @@ class App extends React.Component {
         this.state = {
             firstName: 'Entor',
             lastName: 'Arifi',
-            email: 'arifientor@gmail.com'
+            email: 'arifientor@gmail.com',
+            comment: 'This is my comment',
+            newsletter: false,
+            subscription: 'Platnium',
+            gender: 'female'
         }
+
+        this.handleChange = this.handleChange.bind(this);
     }
     
     handleChange(event) {
-        const name = event.target.name;
-        const value = event.target.value;
+        const { type, name, value, checked } = event.target;
 
         this.setState({
-            [name]: value
-        });
+                [name]: type === 'checkbox' ? checked : value
+            });
+        }
+
+        render() {
+            return (
+                <div>
+                    <h1>My Form</h1>
+                    <form>
+                        <div>
+                            <label>First Name: </label>
+                            <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange} />
+                        </div>
+                        <div>
+                            <label>Last Name: </label>
+                            <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange} />
+                        </div>
+                        <div>
+                            <label>Email: </label>
+                            <input type="text" name="email" value={this.state.email} onChange={this.handleChange} />
+                        </div>
+                        <div>
+                            <label>Comment: </label>
+                            <textarea name="comment" value={this.state.comment} onChange={this.handleChange}></textarea>
+                        </div>
+                        <div>
+                            <label>Newsletter: </label>
+                            <input type="checkbox" name="newsletter" checked={this.state.newsletter} onChange={this.handleChange} />
+                        </div>
+                        <div>
+                            <label>Subscription: </label>
+                            <select name="subscription" value={this.state.subscription} onChange={this.handleChange}>
+                                <option>Silver</option>
+                                <option>Gold</option>
+                                <option>Platnium</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label>Gender: </label>
+                            <input type="radio" name="gender" value="female" onChange={this.handleChange} checked={this.state.gender === 'female'} /> Female
+                            <input type="radio" name="gender" value="male" onChange={this.handleChange} checked={this.state.gender === 'male'} /> Male
+                        </div>
+                        <h2>{this.state.firstName} {this.state.lastName} {this.state.email}</h2>
+                        <h2>{this.state.comment} {this.state.newsletter ? 'checked' : 'unchecked'} {this.state.subscription} {this.state.gender}</h2>
+                        <div>
+                            <button type="submit">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            )
+        }
     }
 
-    render() {
-        return (
-            <div>
-                <h1>My Form</h1>
-                <form>
-                    <div>
-                        <label>First Name: </label>
-                        <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange.bind(this)} />
-                    </div>
-                    <div>
-                        <label>Last Name: </label>
-                        <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange.bind(this)} />
-                    </div>
-                    <div>
-                        <label>Email: </label>
-                        <input type="text" name="email" value={this.state.email} onChange={this.handleChange.bind(this)} />
-                    </div>
-                    <h2>{this.state.firstName} {this.state.lastName} {this.state.email}</h2>
-                    <div>
-                        <button type="submit">Submit</button>
-                    </div>
-                </form>
-            </div>
-        )
-    }
-}
-
-export default App;
+    export default App;
