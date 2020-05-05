60a48b09adfab0dc0db7b341e3bb60653d8272c1
Entor Arifi <entor.arifi@jaywalker-digital.ch>
2020-05-05 19:04:06 +0200

    Introduction to Forms and controlled components.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
 import React from 'react';
-import axios from 'axios';
 
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            users: [],
-            loading: false
+            firstName: 'Entor'
         }
     }
-
-    componentDidMount() {
-        // Fetching data from external sources should be performed inside componentDidMount method
-        this.setState({ loading: true });
-        axios
-            .get('https://jsonplaceholder.typicode.com/users')
-            .then(result => {
-                console.log(result);
-                this.setState({
-                    users: result.data,
-                    loading: false
-                });
-            });
+    
+    handleChange(event) {
+        this.setState({
+            firstName: event.target.value
+        });
     }
 
     render() {
-        const users = this.state.users.map(user => <li>{user.id} - {user.name} {user.phone}</li>)
-
         return (
             <div>
-                {this.state.loading ? <p>Loading...</p> : <ul>{users}</ul>}
+                <h1>My Form</h1>
+                <form>
+                    <div>
+                        <label>First Name: </label>
+                        <input type="text" value={this.state.firstName} onChange={this.handleChange.bind(this)} />
+                    </div>
+                    <h2>{this.state.firstName}</h2>
+                    <div>
+                        <button type="submit">Submit</button>
+                    </div>
+                </form>
             </div>
         )
     }
