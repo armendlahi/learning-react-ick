8f80052cebd428bb395b474e5196b2f09d6f3787
Entor Arifi <entor.arifi@jaywalker-digital.ch>
2020-05-07 17:57:25 +0200

    Increment and decrement example using setState.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import React, { useState } from 'react';
 
-const App = () => {
-    const [name] = useState('Other Name');
-    console.log(name);
+const style = {
+    fontSize: 40,
+    width: 100,
+    margin: '0 auto',
+    textAlign: 'center'
+};
 
-    return <h1>Hello, {name}</h1>
+const App = () => {
+    // const state = useState(1);
+    // const count = state[0];
+    // const setCount = state[1];
+    const [count, setCount] = useState(1);
+
+    // console.log(myFunc(5));
+
+    const changeCount = action => {
+        setCount(prevState => action === 'increment' ? prevState + 1 : prevState - 1);
+
+        // action === 'increment'
+        // if (action === 'increment') {
+        //     setCount(prevCount => prevCount + 1);
+        // } else if (action === 'decrement') {
+        //     setCount(prevCount => prevCount - 1);
+        // } else {
+        //     throw new Error('Invalid parameter');
+        // }
+    }
+    // function increment() {
+    //     setCount(prevCount => prevCount + 1);
+    // }
+
+    return (
+        <div style={style}>
+            <h1>{count}</h1>
+            <button onClick={() => changeCount('increment')}>Increment</button>
+            <button onClick={() => changeCount('decrement')}>Decrement</button>
+        </div>
+    )
 };
 
 // class App extends React.Component {
